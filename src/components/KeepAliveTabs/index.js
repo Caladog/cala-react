f3c32b944ec34540692e0aa2337954e3205004bf
xiaofei266 <xiaofei266@qq.com>
2021-08-06 09:51:54 +0800

    no message

--- src/components/KeepAliveTabs/index.js
+++ src/components/KeepAliveTabs/index.js
@@ -8,7 +8,7 @@ import { CloseCircleOutlined } from '@ant-design/icons';
 export default () => {
   const history = useHistory();
   const location = useLocation();
-  const { getCachingNodes, dropScope,refresh } = useAliveController();
+  const { getCachingNodes, dropScope, refresh } = useAliveController();
   const cachingNodes = getCachingNodes();
 
   const closable = cachingNodes.length > 1;
@@ -25,8 +25,8 @@ export default () => {
     <Tabs size="small" activeKey={location.pathname} type={closable ? 'editable-card' : 'card'} hideAdd className="cala-Tabs">
       {cachingNodes.map((item) => (
         <TabPane key={item.path} tab={
-          <li onClick={() => { history.push(item.path);}} onDoubleClick={() =>{ refresh(item.name)}}>
-            <span style={{userSelect:'none'}}> {item.name} </span>
+          <li onClick={() => { history.push(item.path); }} onDoubleClick={() => { refresh(item.name) }}>
+            <span style={{ userSelect: 'none' }}> {item.name} </span>
           </li>
         } closeIcon={(<CloseCircleOutlined onClick={() => { dropTab(item) }} />)} />
       ))}
